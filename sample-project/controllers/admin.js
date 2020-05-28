1677bac5f39d04657e3b94542ba53ea38fbb11c9
TrangSon <tranghongson@gmail.com>
2020-05-28 18:08:55 +0700

    28/05/2020

--- sample-project/controllers/admin.js
+++ sample-project/controllers/admin.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+// config mongodb
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb+srv://sonkk:pwd@clustersgp-tj3gl.mongodb.net/sproject";
 // routes
 router.get('/', (req, res) => {
   res.redirect('login');
@@ -14,8 +17,6 @@ router.get('/login', (req, res) => {
 router.post('/login', (req, res) => {
   var username = req.body.username;
   var password = req.body.password;
-  var MongoClient = require('mongodb').MongoClient;
-  var uri = "mongodb+srv://sonkk:pwd@clustersgp-tj3gl.mongodb.net/sproject";
   MongoClient.connect(uri, (err, conn) => {
     if (err) throw err;
     var db = conn.db("sproject");
@@ -43,7 +44,87 @@ router.get('/logout', (req, res) => {
   delete req.session.admin;
   res.redirect('login');
 });
-router.get('/category', (req, res) => {
-  res.render('../views/admin/category.ejs');
+router.get('/listcategory', (req, res) => {
+  if (req.session.admin) {
+    MongoClient.connect(uri, (err, conn) => {
+      if (err) throw err;
+      var db = conn.db("sproject");
+      var query = {};
+      db.collection("categories").find(query).toArray((err, result) => {
+        if (err) throw err;
+        res.render('../views/admin/listcategory.ejs', {cates: result});
+        conn.close();
+      });
+    });
+  } else {
+    res.redirect('login');
+  }
+});
+router.get('/addcategory', (req, res) => {
+  res.render('../views/admin/addcategory.ejs');
+});
+router.post('/addcategory', (req, res) => {
+  var cname = req.body.catename;
+  MongoClient.connect(uri, (err, conn) => {
+    if (err) throw err;
+    var db = conn.db("sproject");
+    var cate = { name: cname };
+    db.collection("categories").insertOne(cate, (err, result) => {
+      if (err) throw err;
+      if (result.insertedCount > 0) {
+        res.redirect('listcategory');
+      } else {
+        res.redirect('addcategory');
+      }
+      conn.close();
+    });
+  });
+});
+router.get('/deletecategory', (req, res) => {
+  var id = req.query.id;
+  //console.log(id); // for DEBUG
+  MongoClient.connect(uri, (err, conn) => {
+    if (err) throw err;
+    var db = conn.db("sproject");
+    var query = { _id: require('mongodb').ObjectId(id) };
+    db.collection("categories").deleteOne(query, (err, result) => {
+      if (err) throw err;
+      res.redirect('listcategory');
+      conn.close();
+    });
+  });
+});
+router.get('/editcategory', (req, res) => {
+  var id = req.query.id;
+  MongoClient.connect(uri, (err, conn) => {
+    if (err) throw err;
+    var db = conn.db("sproject");
+    var query = { _id: require('mongodb').ObjectId(id) };
+    db.collection("categories").findOne(query, (err, result) => {
+      if (err) throw err;
+      res.render('../views/admin/editcategory.ejs', {cate: result});
+      conn.close();
+    });
+  });
+});
+router.post('/editcategory', (req, res) => {
+  var id = req.body.id;
+  var cname = req.body.catename;
+  //console.log(id + " | " + cname);
+  MongoClient.connect(uri, (err, conn) => {
+    if (err) throw err;
+    var db = conn.db("sproject");
+    var query = { _id: require('mongodb').ObjectId(id) };
+    var newvalues = { $set: { name: cname } };
+    db.collection("categories").updateOne(query, newvalues, (err, result) => {
+      if (err) throw err;
+      if (result.result.nModified > 0) {
+        res.redirect('listcategory');
+      } else {
+        res.redirect('editcategory');
+      }
+      conn.close();
+    });
+  });
 });
 module.exports = router;
